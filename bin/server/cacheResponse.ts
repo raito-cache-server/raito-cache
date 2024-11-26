4e7905ef2c1566e83c1befbcaafa73639d2197a1
Kiril Yakymchuk <stbestich@gmail.com>
2024-11-26 16:06:51 +0200

    fix: request body handling, refactoring

--- bin/server/cacheResponse.ts
+++ bin/server/cacheResponse.ts
@@ -1,7 +1,8 @@
 import { Context } from 'hono';
 import { cacheStore, Cache } from '../cache';
+import { getKey } from '../utils/getKey';
 
 export const cacheResponse = async (c: Context, body: any) => {
-  const key = c.req.path;
+  const key = getKey(c);
   cacheStore.set(new Cache(key, body));
 };
---
+++ bin/utils/getKey.ts
@@ -0,0 +1,5 @@
+import { Context } from 'hono';
+
+export const getKey = (c: Context) => {
+  return `${c.req.method}:${c.req.path}`;
+}
