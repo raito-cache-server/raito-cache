b12d7e1804b7273986df2a5136340fef30981cf9
Kiril Yakymchuk <stbestich@gmail.com>
2024-11-28 21:44:47 +0200

    refactor: updated get cache logic, types and cli,

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,11 +1,17 @@
 import { InvalidOptionArgumentError, program } from 'commander';
 import { CliOptions } from './types';
-import { listen } from './server';
+import { client } from './client';
+import { server } from './server';
 
 program
   .name('raito-cache')
   .description('Lite caching proxy server')
   .option('--port <port>', 'define port on which to start the server')
+  .option(
+    '--host <host>',
+    'define host on which to start the server',
+    'localhost',
+  )
   .option('--origin <url>', 'define url for caching')
   .version('0.0.1', '-v, --version')
   .parse(process.argv);
@@ -28,7 +34,9 @@ const parseRequiredOption = (
 (async () => {
   try {
     parseRequiredOption(options, ['port', 'origin']);
-    await listen(options);
+    await server.listen(options);
+    console.log(`Ratio-Cache Server started. Type "help" for commands`);
+    client.handleCommand();
   } catch (e) {
     console.error((e as Error).message);
     process.exit(1);
--- bin/types/cacheStore.ts
+++ bin/types/cacheStore.ts
@@ -2,6 +2,6 @@ import { ICache } from './cache';
 
 export interface ICacheStore {
   set(data: ICache): void;
-  get(key: string): ICache | null;
+  get(key?: string): ICache | Map<string, ICache> | null;
   clear(): void;
 }
